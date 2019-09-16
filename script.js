24b1925327409e4cee2af57d2416f9fff12e292c
karolinakv <karolinavalentova2@gmail.com>
2019-09-16 21:34:07 +0200

    added namePart function

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ let studentData;
 
 window.onload = processStudentData;
 
+function setButtonActions(){
+    const sortByButtons = document.getElementById('sortByButtons');
+
+    for(let i = 0; i < sortByButtons.children.length; i++) {
+        const currentButton = sortByButtons.children[i];
+        currentButton.onclick = () => {
+            sortBy(currentButton.textContent);
+        }
+    }
+}
+
 async function processStudentData() {
     try {
+        setButtonActions();
         // Calling fetch within a function in order to pass the returned data to .json() to be converted into a javascript object
         const jsonStudentData = await (await fetch("http://petlatkea.dk/2019/hogwartsdata/students.json")).json();
 
         studentData = jsonStudentData ? jsonStudentData : [];
+        showStudentData(studentData);
+        divideNameParts();
 
     } catch(error) {
         studentData = [];
@@ -27,4 +41,71 @@ function showStudentData(studentsArray) {
 
         studentListElement.appendChild(temporaryStudentEntryTemplate);
     })
+}
+
+function divideNameParts() {
+    studentData.forEach((entry) => {
+        const fullName = entry['fullname'];
+        let array = fullName.split(' ');
+        const lastName = (array.length < 2) ? 'NoLastName' : array[array.length - 1];
+        const firstName = array[0];
+        if(array.length > 2) {
+            const middleName = array.filter( (name) => {
+                if(name.includes(lastName)) {
+                    return false
+                } else if (name.includes(firstName)) {
+                    return false
+                } else return true
+            })
+        }
+    });
+}
+
+
+function sortBy(typeOfSorting) {
+    switch(typeOfSorting) {
+        case 'First Name': {
+            // Sort by first name; source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+            let sortedArray = studentData.sort((a,b) => {
+                if (a['fullname'] < b['fullname']) return -1;
+                if (a['fullname'] > b['fullname']) return 1;
+                return 0;
+            });
+            deleteChilds(document.getElementById('studentList'));
+            showStudentData(sortedArray);
+            break;
+        }
+        case 'Last name': {
+            let sortedArray = studentData.sort((a,b) => {
+                if (a['fullname'] > b['fullname']) return -1;
+                if (a['fullname'] < b['fullname']) return 1;
+                return 0;
+            });
+            deleteChilds(document.getElementById('studentList'));
+            showStudentData(sortedArray);
+            break;
+        }
+        case 'House': {
+            let sortedArray = studentData.sort((a,b) => {
+                if (a['house'] < b['house']) return -1;
+                if (a['house'] > b['house']) return 1;
+                return 0;
+            });
+            deleteChilds(document.getElementById('studentList'));
+            showStudentData(sortedArray);
+            break;
+        }
+        default: {
+            return;
+        }
+    }
+};
+
+// Delete child
+function deleteChilds(parentElement) {
+    let child = parentElement.lastElementChild;
+    while(child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
 }

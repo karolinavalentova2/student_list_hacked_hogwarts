fa0bc4a72f7d64cf395e1eb6862f52e3fbe16edb
karolinakv <karolinavalentova2@gmail.com>
2019-09-18 15:29:36 +0200

    added collapsible in main menu

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+window.onload = start;
+function start(){
+    let coll = document.getElementsByClassName("collapsible");
+
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+}
+
 // let studentData;
 //
 // window.onload = processStudentData;
